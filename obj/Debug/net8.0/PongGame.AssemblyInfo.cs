b6d45822a90b105c7e281a43cdf75023ee091b90
bax082024 <bax082024@gmail.com>
2024-10-23 21:21:51 +0200

    added paddle and balls.cs files

--- obj/Debug/net8.0/PongGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/PongGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PongGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1ca2ab00b913d2719b3f40752da5cedee929ef7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4ee0436b92e86d47bfe323dae5c0c62c64c5d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PongGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PongGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
