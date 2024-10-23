392c05443a2bdcb4f4de9264b880b558221c765d
bax082024 <bax082024@gmail.com>
2024-10-23 22:06:11 +0200

    added gameservice class

--- Services/GameService.cs
+++ Services/GameService.cs
@@ -1,3 +1,24 @@
 using System;
 using PongGame.Models;
 
+namespace PongGame.Services
+{
+  public class GameService
+  {
+    private const int ScreenWidth = 40;
+    private const int ScreenHeight = 20;
+    private Paddle leftPaddle;
+    private Paddle rightPaddle;
+    private Ball ball;
+    private Random random;
+  }
+
+  public GameService()
+  {
+    leftPaddle = new Paddle(ScreenHeight / 2, ScreenHeight);
+    rightPaddle = new Paddle(ScreenHeight / 2, ScreenHeight);
+    ball = new Ball(ScreenWidth / 2, ScreenHeight / 2);
+    random = new Random();
+  }
+
+}
