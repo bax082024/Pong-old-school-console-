91c8d5748f627440b93624ac1730bcc40e7ffd4f
bax082024 <bax082024@gmail.com>
2024-10-23 22:38:02 +0200

    added added void Moveball

--- Services/GameService.cs
+++ Services/GameService.cs
@@ -11,25 +11,75 @@ namespace PongGame.Services
     private Paddle rightPaddle;
     private Ball ball;
     private Random random;
-  }
+  
 
-  public GameService()
-  {
-    leftPaddle = new Paddle(ScreenHeight / 2, ScreenHeight);
-    rightPaddle = new Paddle(ScreenHeight / 2, ScreenHeight);
-    ball = new Ball(ScreenWidth / 2, ScreenHeight / 2);
-    random = new Random();
-  }
-
-  public void Start()
-  {
-    while (true)
+    public GameService()
     {
-      HandleInput();
-      MoveAll();
-      Draw();
-      System.Threading.Thread.Sleep(100);
+      leftPaddle = new Paddle(ScreenHeight / 2, ScreenHeight);
+      rightPaddle = new Paddle(ScreenHeight / 2, ScreenHeight);
+      ball = new Ball(ScreenWidth / 2, ScreenHeight / 2);
+      random = new Random();
     }
-  }
 
+    public void Start()
+    {
+      while (true)
+      {
+        HandleInput();
+        MoveAll();
+        Draw();
+        System.Threading.Thread.Sleep(100);
+      }
+    }
+
+    private void HandleInput()
+    {
+      if (Console.KeyAvailable)
+      {
+        var key = Console.ReadKey(true).Key;
+        switch (key)
+        {
+          case ConsoleKey.W:
+            leftPaddle.MoveUp();
+            break;
+
+          case ConsoleKey.S:
+            leftPaddle.MoveDown();
+            break;
+
+          case ConsoleKey.UpArrow:
+            rightPaddle.MoveUp();
+            break;
+
+          case ConsoleKey.DownArrow:
+            rightPaddle.MoveDown();
+            break;
+
+        }
+      }
+    }
+
+    private void MoveBall()
+    {
+      ball.Move();
+
+      if (ball.Y <= 0 || ball.Y >= ScreenHeight - 1)
+      {
+        ball.BounceVertical();
+      }
+
+      if (ball.X <= 1 && ball.Y == leftPaddle.Position || ball.X >= ScreenWidth - 2 && ball.Y == rightPaddle.Position)
+      {
+        ball.BounceHorizontal();
+      }
+
+      if (ball.X <= 0 || ball.X >= ScreenWidth - 1)
+      {
+        Console.Clear();
+        Console.WriteLine(ball.X <= 0 ? "Right Player Wins!!!" : "Left Player Wins!!!");
+        Environment.Exit(0);
+      }
+    }
+
+  }
 }
