7ffc876182a855247b58684fc4434296f9e709c1
bax082024 <bax082024@gmail.com>
2024-10-23 21:49:41 +0200

    added ball class

---
+++ Models/balls.cs
@@ -0,0 +1,23 @@
+namespace PongGame.Models
+{
+  public class Ball
+  {
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int XDirection { get; set; }
+    public int YDirection { get; set; }
+
+    public Ball(int startX, int startY)
+    {
+      X = startX;
+      Y = startY;
+      XDirection = 1;
+      XDirection = 1;
+    }
+
+    public void BounceHorizontal() => XDirection *= -1;
+
+
+  }
+
+}
